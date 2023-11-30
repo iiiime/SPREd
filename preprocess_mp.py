ffc25d4b11cfeaa154bef412f5f86239c89a03e0
Zijun Wu <zijnuw@gmail.com>
2023-11-30 14:12:46 -0500

    parser defaults updated

--- preprocess_mp.py
+++ preprocess_mp.py
@@ -23,10 +23,10 @@ from SERGIO.sergio import sergio
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file_id', type=int, default=0, help='file id of hist, cov, and label')
-parser.add_argument('--save', type=str, default='100_feature_50_cond_3_5_mr_5input_coxbox/', help='directory to save input files for neural network')
-parser.add_argument('--data_dir', type=str, default='./dataset/100_feature_100_cond_3_5_mr_5input/', help='directory of SERGIO simulated data files')
+parser.add_argument('--save', type=str, default='./dataset/', help='directory to save input files for neural network')
+parser.add_argument('--data_dir', type=str, default='./dataset/data/', help='directory of SERGIO simulated data files')
 parser.add_argument('--n_cond', type=int, default=50, help='number of conditions in expression matrix; number of cells in SERGIO simulations')
-parser.add_argument('--n_bins', type=int, default=8, help='bin number of 2d histograms for mi calculation')
+parser.add_argument('--n_bins', type=int, default=6, help='bin number of 2d histograms for mi calculation')
 parser.add_argument('--n_samples', type=int, default=10, help='number of data points for nn models')
 parser.add_argument('--n_mrs', type=int, default=5, help='number of master regulators')
 parser.add_argument('--n_features', type=int, default=100, help='number of tfs')
